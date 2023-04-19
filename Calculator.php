6a27d4ad25c94cb9329de4faf32b4ecfdb7fb9c1
Maite Chirivella <mt.chirivellaquesa@edu.gva.es>
2023-04-19 21:59:23 +0200

    Funcion par

---
+++ Calculator.php
@@ -0,0 +1,25 @@
+<?php
+class Calculator
+{
+ 
+    public function add($a, $b): float
+    {
+        return $a + $b;
+    }
+
+    public function multiply($a, $b): float
+    {
+        return $a * $b;
+    }
+
+    public function division($a, $b): float
+    {
+        return $a / $b;
+    }
+     public function par($num): boolean
+    {
+        if($num%2==0)return true;
+        else return false;
+    }
+ 
+}
